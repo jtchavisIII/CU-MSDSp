2c5a0f1196c555e044a0f9c2b7f1516eb717e929
John <jtc278@cornell.edu>
2020-08-18 15:01:09 -0400

    Adding license info

--- src/plotMCMCResults.py
+++ src/plotMCMCResults.py
@@ -5,13 +5,27 @@ NAME: plotCRjrmcmc.py
 DESC: This script will trace plots for poisson MCMC. 
 
 
-NOTES 
-2020-AUG-02 Eventually I need to include the warm-up periods....Not right now
-			But before we publish....
+MIT License
 
-			+ Need to handle the burn in issue for Phase two cralwer vs trace plot
-			+ Missing critial piece. Only have $Sapmle len...is burn in an issue
-			+ Have users decide if they want to show plots
+Copyright (c) 2020 John T. Chavis III
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 
